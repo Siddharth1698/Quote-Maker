b63d0dbc575031390fb6a92097407ff4d725f754
Siddharth M <siddharth.m98@gmail.com>
2018-06-05 11:41:44 +0530

    Little Changes

--- app/src/main/java/com/example/siddharthm/quotemaker/ColorSelection.java
+++ app/src/main/java/com/example/siddharthm/quotemaker/ColorSelection.java
@@ -1,10 +1,13 @@
 package com.example.siddharthm.quotemaker;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class ColorSelection extends AppCompatActivity {
     Button white;
@@ -19,6 +22,7 @@ public class ColorSelection extends AppCompatActivity {
     Button red;
     Button lightred;
     Button black;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +40,153 @@ public class ColorSelection extends AppCompatActivity {
         red = (Button)findViewById(R.id.button12);
         lightred = (Button)findViewById(R.id.button13);
         black = (Button)findViewById(R.id.button14);
+        editText = (EditText)findViewById(R.id.editText);
+        final LinearLayout l = (LinearLayout)findViewById(R.id.l);
+
+
 
         white.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),QuoteMaking.class);
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
                 i.putExtra("color","white");
+                i.putExtra("quote",s);
                 startActivity(i);
             }
         });
+
+        gray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","gray");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        lightblue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","lightblue");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","blue");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","green");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        orange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","orange");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        lightorange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","lightorange");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        purple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","purple");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","red");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        lightred.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","lightred");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        lightgreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","lightgreen");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+        black.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String s = editText.getText().toString();
+                Intent i = new Intent(getApplicationContext(),FinalScreen.class);
+                i.putExtra("color","black");
+                i.putExtra("quote",s);
+                startActivity(i);
+            }
+        });
+
+
 
     }
 }
--- app/src/main/java/com/example/siddharthm/quotemaker/FinalScreen.java
+++ app/src/main/java/com/example/siddharthm/quotemaker/FinalScreen.java
@@ -1,20 +1,29 @@
 package com.example.siddharthm.quotemaker;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class FinalScreen extends AppCompatActivity {
     TextView t;
+    LinearLayout l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_screen);
         t = (TextView)findViewById(R.id.textView);
+        l = (LinearLayout)findViewById(R.id.lay);
         Bundle extras = getIntent().getExtras();
         String quoteq = extras.getString("quote");
+        String colorr = extras.getString("color");
         t.setText(quoteq);
+        if(colorr == "gray") {
+            l.setBackgroundColor(Color.GRAY);
+        }
 
-    }
+
+        }
 }
